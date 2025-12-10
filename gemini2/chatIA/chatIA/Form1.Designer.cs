30f5ace3ec870624847426f271e5c07037fed66e
Kauan Borges <kauanborgesd05@gmail.com>
2025-12-09 23:05:26 -0300

    subindo arquivos
    Subindo projeto local - V1

---
+++ gemini2/chatIA/chatIA/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace chatIA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEntrada = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSaida = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.lblErro = new System.Windows.Forms.Label();
+            this.txtHistorico = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtEntrada
+            // 
+            this.txtEntrada.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtEntrada.Location = new System.Drawing.Point(12, 79);
+            this.txtEntrada.Multiline = true;
+            this.txtEntrada.Name = "txtEntrada";
+            this.txtEntrada.Size = new System.Drawing.Size(419, 65);
+            this.txtEntrada.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(8, 278);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ÚLTIMA RESPOSTA:";
+            // 
+            // txtSaida
+            // 
+            this.txtSaida.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtSaida.Location = new System.Drawing.Point(12, 305);
+            this.txtSaida.Multiline = true;
+            this.txtSaida.Name = "txtSaida";
+            this.txtSaida.ReadOnly = true;
+            this.txtSaida.Size = new System.Drawing.Size(419, 96);
+            this.txtSaida.TabIndex = 2;
+            this.txtSaida.TextChanged += new System.EventHandler(this.txtSaida_TextChanged);
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.Location = new System.Drawing.Point(12, 150);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(108, 35);
+            this.btnEnviar.TabIndex = 4;
+            this.btnEnviar.Text = "ENVIAR";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // lblErro
+            // 
+            this.lblErro.AutoSize = true;
+            this.lblErro.ForeColor = System.Drawing.Color.Red;
+            this.lblErro.Location = new System.Drawing.Point(600, 305);
+            this.lblErro.Name = "lblErro";
+            this.lblErro.Size = new System.Drawing.Size(0, 13);
+            this.lblErro.TabIndex = 5;
+            // 
+            // txtHistorico
+            // 
+            this.txtHistorico.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txtHistorico.Location = new System.Drawing.Point(480, 30);
+            this.txtHistorico.Multiline = true;
+            this.txtHistorico.Name = "txtHistorico";
+            this.txtHistorico.ReadOnly = true;
+            this.txtHistorico.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtHistorico.Size = new System.Drawing.Size(523, 423);
+            this.txtHistorico.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(139, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(176, 33);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "GEMINI 2.0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(8, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ENTRADA:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(476, 3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 24);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "HISTÓRICO:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1015, 465);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtHistorico);
+            this.Controls.Add(this.lblErro);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSaida);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEntrada);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEntrada;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSaida;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Label lblErro;
+        private System.Windows.Forms.TextBox txtHistorico;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ gemini2/chatIA/chatIA/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chatIA
+{
+    public partial class Form1 : Form
+    {
+        private const string ModelName = "gemini-2.0-flash";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnEnviar_Click(object sender, EventArgs e)
+        {
+            lblErro.Text = "";
+            txtSaida.Text = "";
+
+
+            string apiKey = "AIzaSyAwZJOotZ_QuCtmetgQQ11_g0RaZoeB0FY";
+
+            if (string.IsNullOrWhiteSpace(txtEntrada.Text))
+            {
+                lblErro.Text = "Digite uma mensagem na entrada.";
+                return;
+            }
+
+            //historico papai
+            txtHistorico.AppendText("Você: " + txtEntrada.Text + Environment.NewLine);
+
+
+            //chamando o gemini aq
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string url =
+                        $"https://generativelanguage.googleapis.com/v1beta/models/{ModelName}:generateContent?key={apiKey}";
+
+                    var body = new
+                    {
+                        contents = new[]
+                        {
+                            new {
+                                parts = new[] { new { text = txtEntrada.Text } },
+                                role = "user"
+                            }
+                        }
+                    };
+
+                    //serializando a resposta 
+                    string jsonBody = JsonSerializer.Serialize(body);
+                    var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+                    HttpResponseMessage response = await client.PostAsync(url, content);
+                    string responseText = await response.Content.ReadAsStringAsync();
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        lblErro.Text = "Erro da IA: " + responseText;
+                        return;
+                    }
+
+                    // exibindo a resposta dps de extraida e capturada
+                    string textoGerado = ExtrairTexto(responseText);
+
+                    txtSaida.Text = textoGerado;
+
+                    // historico da ia
+                    txtHistorico.AppendText("IA: " + textoGerado + Environment.NewLine + Environment.NewLine);
+                }
+            }
+            catch (Exception ex)
+            {
+                lblErro.Text = "Erro: " + ex.Message;
+            }
+
+            txtEntrada.Text = "";
+        }
+
+        
+        //extraindo o json da resposta
+        private string ExtrairTexto(string json)
+        {
+            try
+            {
+                var doc = JsonDocument.Parse(json);
+
+                var candidates = doc.RootElement
+                    .GetProperty("candidates")[0]
+                    .GetProperty("content")
+                    .GetProperty("parts")[0]
+                    .GetProperty("text");
+
+                return candidates.GetString();
+            }
+            catch
+            {
+                return "Erro ao interpretar a resposta da IA.";
+            }
+        }
+
+        private void txtSaida_TextChanged(object sender, EventArgs e)
+        {
+            txtSaida.ReadOnly = true;
+        }
+    }
+}
